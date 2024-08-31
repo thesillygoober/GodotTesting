1a5704c15334d5f8d63b5f9dd845a549987dbb93
thesillygoober <70186143+thesillygoober@users.noreply.github.com>
2024-08-31 12:58:25 -0400

    Network Test Update
    Added a network test scene, committing this to go try it through LAN. More commits will come for this scene.

---
+++ NetworkTest/network_test.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+var has_created_server: bool = false
+var server: ENetMultiplayerPeer
+
+var has_created_client: bool = false
+var client: ENetMultiplayerPeer
+
+func _ready():
+
+    multiplayer.peer_connected.connect(_on_client_connected)
+    multiplayer.peer_disconnected.connect(_on_client_disconnected)
+    multiplayer.connected_to_server.connect(_on_connected_ok)
+    multiplayer.connection_failed.connect(_on_connection_fail)
+
+func _on_client_connected(id):
+
+    print("Client has connected: " + str(id))
+
+func _on_client_disconnected(id):
+
+    print("Client has disconnected: " + str(id))
+
+func _on_connected_ok():
+
+    print("Connected to server successfully.")
+
+func _on_connection_fail():
+
+    print("Failed connecting to server.")
+
+func _on_create_server_pressed():
+
+    if not has_created_server and not has_created_client:
+
+        has_created_server = true
+
+        server = ENetMultiplayerPeer.new()
+        server.create_server(1336, 8)
+        multiplayer.multiplayer_peer = server
+
+        print("Server ID: " + str(multiplayer.get_unique_id()))
+        print("Is Server: " + str(multiplayer.is_server()))
+
+func _on_join_server_pressed():
+	
+    if not has_created_client and not has_created_server:
+
+        has_created_client = true
+
+        var desired_address_target: String = $CanvasLayer/Control/PanelContainer/VBoxContainer/HBoxContainer/IPAddress.text
+
+        client = ENetMultiplayerPeer.new()
+        client.create_client(desired_address_target, 1336)
+        multiplayer.multiplayer_peer = client
